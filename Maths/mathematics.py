af7512378637d1326df921cf83f132d0d9820407
Collin Campbell <collinc@gmail.com>
2024-06-07 12:21:06 -0700

    Lab: Added mathematical functions.

---
+++ Maths/mathematics.py
@@ -0,0 +1,15 @@
+# Task 1: Write the mathematical functions
+
+def summation(a, b):
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+def multiplication(a, b):
+    result = a * b
+    return result
+
+
